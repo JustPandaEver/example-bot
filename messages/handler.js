b48ea18443c89e4253e9d755052dba6ec4a20710
Akkun3704 <75048902+Akkun3704@users.noreply.github.com>
2021-08-05 18:06:00 +0700

    update handler.js

--- messages/handler.js
+++ messages/handler.js
@@ -164,7 +164,7 @@ module.exports = {
 						if (!img) return conn.reply(from, `Reply gambar dengan caption ${prefix + command}`, msg)
 						conn.sendImageAsSticker(from, img.toString('base64'), msg, { pack: pack[0] ? pack[0] : msg.pushname, author: pack[1] ? pack[1] : isGroup ? groupMetadata.subject : "︎ ︎ ︎" })
 					} else if (msg.isVideo || msg.isQuotedVideo) {
-						if (msg.message[msg.type].seconds < 11 || quotedMsg[quotedMsg.type].seconds < 11) return conn.reply(from, 'Maksimal 10 detik!', msg)
+						if (msg.message[msg.type].seconds > 11 || quotedMsg[quotedMsg.type].seconds > 11) return conn.reply(from, 'Maksimal 10 detik!', msg)
 						let img = isQuotedMsg ? await quotedMsg.toBuffer() : await msg.toBuffer()
 						if (!img) return conn.reply(from, `Reply video/gif dengan caption ${prefix + command}`, msg)
 						conn.sendMp4AsSticker(from, img.toString('base64'), msg, { pack: pack[0] ? pack[0] : msg.pushname, author: pack[1] ? pack[1] : isGroup ? groupMetadata.subject : "︎ ︎ ︎" })
