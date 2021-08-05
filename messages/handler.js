9796f7ddb9b09d45b37763045357773b96e12b8d
Akkun3704 <75048902+Akkun3704@users.noreply.github.com>
2021-08-05 17:30:54 +0700

    update handler.js

--- messages/handler.js
+++ messages/handler.js
@@ -161,7 +161,7 @@ module.exports = {
 					if (msg.isImage || msg.isQuotedImage) {
 						let img = isQuotedMsg ? await quotedMsg.toBuffer() : await msg.toBuffer()
 						if (!img) return conn.reply(from, `Reply gambar dengan caption ${prefix + command}`, msg)
-						conn.sendImageAsSticker(from, img.toString('base64'), msg, { pack: pack[0] ? pack[0] : msg.pushname, author: pack[1] ? pack[1] : groupMetadata.subject })
+						conn.sendImageAsSticker(from, img.toString('base64'), msg, { pack: pack[0] ? pack[0] : msg.pushname, author: pack[1] ? pack[1] : isGroup ? groupMetadata.subject : author })
 					} else if (msg.isVideo || msg.isQuotedVideo) {
 						if (msg.message[msg.type].seconds < 11 || quotedMsg[quotedMsg.type].seconds < 11) return conn.reply(from, 'Maksimal 10 detik!', msg)
 						let img = isQuotedMsg ? await quotedMsg.toBuffer() : await msg.toBuffer()
